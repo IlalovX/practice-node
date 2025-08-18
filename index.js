3ea0a2db82e3a7aab73735139bbf4dcf17e9c053
IlalovX <ilalovx@gmail.com>
2025-08-19 00:11:26 +0500

    add prod

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ require('./startup/db')();
 
 require('./startup/routes')(app);
 
+require('./startup/prod')(app);
+
 const server = app.listen(5000, () =>
   winston.info('Listening on port 5000...')
 );
